1c8b9f468c5d1b387858e471b9376d5d5c37f5a5
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-05-18 16:20:29 +0200

    Listing items

--- market.py
+++ market.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template("home.html")
+
+
+@app.route("/market")
+def market_page():
+    items = [
+        {"id": 1, "name": "Phone", "barcode": "893212299897", "price": 500},
+        {"id": 2, "name": "Laptop", "barcode": "125481515848", "price": 900},
+        {"id": 3, "name": "Keyboard", "barcode": "166545645678", "price": 150},
+    ]
+    return render_template("market.html", items=items)
