be462320ac66b201906cbfb792b3a438e812538e
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-05-19 12:27:05 +0200

    Add register form and template

--- market/__init__.py
+++ market/__init__.py
@@ -7,10 +7,12 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
+from market import routes
+from market import models
+
 with app.app_context():
     db.create_all()
-
-from market import routes
